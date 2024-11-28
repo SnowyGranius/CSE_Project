b97ddd28101622a439c594ea55b6f904e6edbe1b
Ion388 <ion.stefan2004@gmail.com>
2024-11-28 21:51:23 +0200

    imagini_pentu_cnn

--- Image_dataset_generation/Generate_images.py
+++ Image_dataset_generation/Generate_images.py
@@ -1,8 +1,9 @@
 import numpy as np
-from skimage.draw import disk
+from skimage.draw import disk, polygon
 import matplotlib.pyplot as plt
 import sys
 import os
+from skimage.draw import ellipse
 
 current_directory = os.path.dirname(os.path.abspath(sys.argv[0])) 
 print(current_directory)
@@ -39,22 +40,72 @@ def generate_image(image_name, image_shape=(1000, 1000)):
     y_scaler = (image_shape[0] - 1) / (y_max - y_min)
     print(x_scaler, y_scaler)
 
-    
     x_coord_scaled = (x_coord - x_min) * x_scaler
     y_coord_scaled = (y_coord - y_min) * y_scaler
-    radii_scaled = radii * x_scaler # Or y scaler?
+    radii_scaled = radii * x_scaler  # Or y scaler?
 
-    
     nr_circles = len(x_coord)
     for i in range(nr_circles):
-        rr, cc = disk((x_coord_scaled[i], y_coord_scaled[i]), radii_scaled[i], shape=image_shape)
-        image[rr, cc] = 1 # Foreground
+        rr, cc = ellipse(x_coord_scaled[i], y_coord_scaled[i], radii_scaled[i], radii_scaled[i], shape=image_shape)
+        image[rr, cc] = 1  # Foreground
 
-    
-    plt.imsave(f'{current_directory}/Circle_Images/{image_name}', image, cmap='gray')
+    image_name = image_name.split('.png')[0]
+    plt.imsave(f'{current_directory}/Circle_Images/{image_name}_circle.png', image, cmap='gray')
 
-Models = [1]
-pfs = ['0.1']
+    image = np.zeros(image_shape, dtype=np.uint8)
+    nr_circles = len(x_coord)
+    for i in range(nr_circles):
+        rr, cc = ellipse(x_coord_scaled[i], y_coord_scaled[i], radii_scaled[i]*0.5, radii_scaled[i], shape=image_shape)
+        image[rr, cc] = 1  # Foreground
+
+    image_name = image_name.split('.png')[0]
+    plt.imsave(f'{current_directory}/Circle_Images/{image_name}_ellipse.png', image, cmap='gray')
+
+    image = np.zeros(image_shape, dtype=np.uint8)
+    for i in range(nr_circles):
+        # Calculate rectangle coordinates
+        rect_x_min = int(x_coord_scaled[i] - radii_scaled[i]/np.sqrt(2))
+        rect_x_max = int(x_coord_scaled[i] + radii_scaled[i]/np.sqrt(2))
+        rect_y_min = int(y_coord_scaled[i] - radii_scaled[i]/np.sqrt(2))
+        rect_y_max = int(y_coord_scaled[i] + radii_scaled[i]/np.sqrt(2))
+
+        # Ensure coordinates are within image bounds
+        rect_x_min = max(rect_x_min, 0)
+        rect_x_max = min(rect_x_max, image_shape[1] - 1)
+        rect_y_min = max(rect_y_min, 0)
+        rect_y_max = min(rect_y_max, image_shape[0] - 1)
+
+        # Draw the rectangle
+        image[rect_x_min:rect_x_max, rect_y_min:rect_y_max] = 1
+
+    image_name = image_name.split('.png')[0]
+    plt.imsave(f'{current_directory}/Circle_Images/{image_name}_rectangle.png', image, cmap='gray')
+
+    # Generate isosceles triangle within the ellipse
+    image = np.zeros(image_shape, dtype=np.uint8)
+    for i in range(nr_circles):
+        # Calculate triangle vertices
+        base_half = radii_scaled[i] / np.sqrt(2)
+        height = radii_scaled[i] / np.sqrt(2)
+
+        x_center = x_coord_scaled[i]
+        y_center = y_coord_scaled[i]
+
+        vertices = np.array([
+            [x_center, y_center - height],  # Top vertex
+            [x_center - base_half, y_center + base_half],  # Bottom left vertex
+            [x_center + base_half, y_center + base_half]   # Bottom right vertex
+        ])
+
+        # Draw the triangle
+        rr, cc = polygon(vertices[:, 0], vertices[:, 1], shape=image_shape)
+        image[rr, cc] = 1
+
+    image_name = image_name.split('.png')[0]
+    plt.imsave(f'{current_directory}/Circle_Images/{image_name}_triangle.png', image, cmap='gray')
+
+Models = np.arange(1, 26, 1)
+pfs = ['0.1', '0.2', '0.3', '0.4', '0.5']
 # Models = range(1, 26)
 # pfs = np.arange(1, 11, 1) / 10
 # print(pfs)
