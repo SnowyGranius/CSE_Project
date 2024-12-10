ed59cc45712ebc8971e2542ca32016ac2badb25e
Ion388 <ion.stefan2004@gmail.com>
2024-12-10 21:43:10 +0100

    nopoolcnn_update

--- CNN/classes_cnn.py
+++ CNN/classes_cnn.py
@@ -82,32 +82,38 @@ class NoPoolCNN(nn.Module):
         super().__init__()
         self.layers = nn.Sequential(
 
-            nn.Conv2d(1, 32, kernel_size=2, padding=1), 
+            nn.Conv2d(1, 8, kernel_size=2, padding=1), 
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=3),
+            nn.MaxPool2d(kernel_size=2, stride=2),
 
+            nn.Conv2d(8, 32, kernel_size=2, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
             nn.Conv2d(32, 64, kernel_size=2, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=3),
-            
-            nn.Conv2d(64, 128, kernel_size=2, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=3),
-            
-            nn.Conv2d(128, 256, kernel_size=2, padding=1),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(64, 64, kernel_size=2, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
 
-            nn.Conv2d(256, 256, kernel_size=2, padding=1),
+            nn.Conv2d(64, 64, kernel_size=2, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
 
+            nn.Conv2d(64, 64, kernel_size=2, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(64, 64, kernel_size=2, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
 
             nn.Flatten(),
             ############## MLP LAYERS #############
-            nn.Linear(25600, 800), # 4608, 18432, 51200,
-            nn.ReLU(),
-            nn.Linear(800, 100),
+            nn.Linear(4096, 100), # 4608, 18432, 51200,
             nn.ReLU(),
             nn.Linear(100, 10),
             nn.Linear(10, 1)
